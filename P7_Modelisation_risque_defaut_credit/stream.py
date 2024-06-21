395b5af64cf22608e99d77146950277200b85a7c
YannGwad <90218739+YannGwad@users.noreply.github.com>
2024-06-21 02:14:51 +0200

    Update stream.py

--- P7_Modelisation_risque_defaut_credit/stream.py
+++ P7_Modelisation_risque_defaut_credit/stream.py
@@ -409,7 +409,8 @@ if options == "Profil Client":
         ########################################################
         # Récupération et affichage des informations du client #
         ########################################################
-        df_info_client = pickle.load(open("P7_Modelisation_risque_defaut_credit/pickle_files/df_info_client.pickle", "rb"))
+        #df_info_client = pickle.load(open("P7_Modelisation_risque_defaut_credit/pickle_files/df_info_client.pickle", "rb"))
+        df_info_client = pd.read_csv("P7_Modelisation_risque_defaut_credit/pickle_files/df_info_client.csv")
         df_info_client = df_info_client[df_info_client.SK_ID_CURR == ID_client]
 
         df_pret_client = pickle.load(open("P7_Modelisation_risque_defaut_credit/pickle_files/df_pret_client.pickle", "rb"))
